a0c1834017fe773634d9cb6c454f8705e2938979
Styrbjörn Nordberg <styris.n@gmail.com>
2022-12-12 13:18:14 +0100

    added js built hover function

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const modeTracker = { isDarkmode: true, isLightmode: false };
+var darkContainer = document.querySelector(".darkContainer");
+var lightContainer = document.querySelector(".lightContainer");
 
 function switchMode() {
-  var darkContainer = document.querySelector(".darkContainer");
-  var lightContainer = document.querySelector(".lightContainer");
-
   if (modeTracker.isDarkmode == true) {
     modeTracker.isDarkmode = false;
     modeTracker.isLightmode = true;
@@ -28,7 +27,25 @@ function switchMode() {
   }
 }
 
-const h2s = document.querySelectorAll('.h2');
-h2s.forEach((h2)=> {
-  h2.addEventListener('click', switchMode());
+let h2s = document.querySelectorAll("h2");
+h2s.forEach(function (h2) {
+  h2.addEventListener("click", function () {
+    switchMode();
+  });
+  h2.addEventListener("mouseover", function () {
+    if (this.parentElement == darkContainer) {
+      this.parentElement.classList.add('lightmode');
+    }
+    if (this.parentElement == lightContainer) {
+      this.parentElement.classList.add('darkmode')
+    }
+  });
+  h2.addEventListener("mouseout", function() {
+    if (this.parentElement == darkContainer) {
+      this.parentElement.classList.remove('lightmode');
+    }
+    if (this.parentElement == lightContainer) {
+      this.parentElement.classList.remove('darkmode')
+    }
+  })
 });
